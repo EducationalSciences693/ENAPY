2950ee899456767f554e44eea896c2a63d6fce0c
Mariah A. Knowles <snotskie@gmail.com>
2021-11-20 02:03:22 +0000

    documenting

--- examples/helpers.py
+++ examples/helpers.py
@@ -5,9 +5,12 @@ def ena_plot(enaset, groupby=None, groupmode="facet"):
     if groupmode not in ["facet", "subtract"]:
         raise Exception("groupmode must be either 'facet' or 'subtract'")
     
+    # Start the plot, using plotnine
     points = enaset.rx2("points")
     plot = ggplot(data=points)
     plot += coord_fixed(ratio=1, xlim=(-1, 1), ylim=(-1, 1))
+    
+    # Either show units as black (else) or colored by group (if)
     if groupby is not None:
         plot += geom_point(mapping=aes(x="SVD1", y="SVD2", color=groupby))
         if groupmode == "facet":
@@ -15,6 +18,7 @@ def ena_plot(enaset, groupby=None, groupmode="facet"):
     else:
         plot += geom_point(mapping=aes(x="SVD1", y="SVD2"))
     
+    # Prepare counts and nodes objects for below
     counts = enaset.rx2("line.weights")
     nodes = enaset.rx2("rotation").rx2("nodes")
     if groupby is not None:
@@ -23,6 +27,12 @@ def ena_plot(enaset, groupby=None, groupmode="facet"):
     else:
         mean_points = counts.select_dtypes(include="number").mean()
 
+    # For each possible pair of nodes,
+    # if that pairing is a valid connection in our model,
+    # draw the network line. We draw the line slightly
+    # differently depending on the groupby and groupmode.
+    # Either way, the general strategy is to make a temp
+    # dataframe and plot one line from it.
     for _, node1 in nodes.iterrows():
         for _, node2 in nodes.iterrows():
             connection = f"{node1.code} & {node2.code}"
@@ -43,7 +53,7 @@ def ena_plot(enaset, groupby=None, groupmode="facet"):
                             groups.append(group_name)
                             groups.append(group_name)
                             edges = DataFrame({"SVD1": xs, "SVD2": ys, groupby: groups, "size": sizes})
-                            plot += geom_line(data=edges, mapping=aes(x="SVD1/3", y="SVD2/3", size="size", color=groupby))
+                            plot += geom_line(data=edges, mapping=aes(x="SVD1/3", y="SVD2/3", size="size", alpha="size", color=groupby))
                     else: # subtract
                         xs = []
                         ys = []
@@ -77,6 +87,7 @@ def ena_plot(enaset, groupby=None, groupmode="facet"):
                     edges = DataFrame({"SVD1": xs, "SVD2": ys, "size": sizes})
                     plot += geom_line(data=edges, mapping=aes(x="SVD1/3", y="SVD2/3", size="size", alpha="size"))
 
+    # Finally, draw the node points and labels and we're done
     plot += geom_point(data=nodes, mapping=aes(x="SVD1/3", y="SVD2/3"))
     plot += geom_label(data=nodes, mapping=aes(x="SVD1/3", y="SVD2/3", label="code", size=8))
     return plot
